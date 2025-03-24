8413336ac420bd289a0b7d136dc7d1069ec99c20
loeken <loeken@internetz.me>
2025-03-24 17:05:58 +0100

    poc install k3s via k3sup

---
+++ deploy/terraform/k3s/variables.tf
@@ -0,0 +1,40 @@
+variable "ssh_username" {
+  type = string
+  default = "user"
+  description = "The username to use when connecting to the server via SSH."
+}
+variable "ssh_private_key" {
+  type = string
+  default = "~/.ssh/id_ed25519"
+  description = "The path to the private key to use when connecting to the server via SSH."
+}
+variable "kubernetes_version" {
+    type = string
+    default = "v1.26.4+k3s1"
+    description = "which version of k3s to install, usually 1 versions behind the latest"
+}
+variable "external_ip" {
+    type = string
+    default = "1.2.3.4"
+    description = "sets the external ip address, a script to update ips and restart k3s is also uploaded to the vm"
+}
+variable "ssh_password" {
+  type = string
+  default = "demotime"
+  description = "The password to use when connecting to the server via SSH."
+}
+variable "ssh_public_key" {
+  type = string
+  default = "~/.ssh/id_ed25519.pub"
+  description = "The path to the public key to use when connecting to the server via SSH."
+}
+variable "ssh_server_address" {
+  type = string
+  default = "localhost"
+  description = "The address of the server to connect to via SSH."
+}
+variable "storage" {
+  type = string
+  default = "local-path"
+  description = "the default storage class"
+}
