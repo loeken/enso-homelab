0fcce40ad9945afd798de3b9edac0238b9b1fc42
loeken <loeken@internetz.me>
2025-03-31 14:30:49 +0200

    testing, pre adding nfs support for /mnt/data

--- deploy/terraform/k3s/main.tf
+++ deploy/terraform/k3s/main.tf
@@ -1,23 +1,62 @@
 provider "null" {}
 
+resource "null_resource" "check_hostname" {
+  provisioner "local-exec" {
+    command = <<EOT
+    echo '${var.ssh_private_key}' | base64 -d > /tmp/id_ed25519
+    chmod 600 /tmp/id_ed25519
+
+    EXPECTED_HOSTNAME="${var.hostname}"
+
+    ACTUAL_HOSTNAME=$(ssh -i /tmp/id_ed25519 -p ${var.ssh_server_port} -o StrictHostKeyChecking=no ${var.ssh_username}@${var.ssh_server_address} 'hostname')
+
+    echo "Expected hostname: $EXPECTED_HOSTNAME"
+    echo "Actual hostname:   $ACTUAL_HOSTNAME"
+
+    if [ "$ACTUAL_HOSTNAME" != "$EXPECTED_HOSTNAME" ]; then
+      echo "❌ Hostname does not match!"
+      exit 1
+    else
+      echo "✅ Hostname verified."
+    fi
+
+    rm -f /tmp/id_ed25519
+    EOT
+  }
+}
+
 resource "null_resource" "bootstrap-k3s" {
   provisioner "local-exec" {
     command = <<EOT
     echo '${var.ssh_private_key}' | base64 -d > /tmp/id_ed25519
     chmod 600 /tmp/id_ed25519
 
+    echo "[DEBUG] Running k3sup install with:"
+    echo "/usr/local/bin/k3sup install \\"
+    echo "  --ip ${var.ssh_server_address} \\"
+    echo "  --user ${var.ssh_username} \\"
+    echo "  --ssh-key /tmp/id_ed25519 \\"
+    echo "  --ssh-port ${var.ssh_server_port} \\"
+    echo "  --cluster \\"
+    echo "  --k3s-version ${var.kubernetes_version} \\"
+    echo "  --k3s-extra-args '--disable=traefik --node-external-ip=${var.external_ip} --advertise-address=${var.internal_ip} --node-ip=${var.internal_ip}'"
+
     /usr/local/bin/k3sup install \
       --ip ${var.ssh_server_address} \
       --user ${var.ssh_username} \
       --ssh-key /tmp/id_ed25519 \
+      --ssh-port ${var.ssh_server_port} \
       --cluster \
       --k3s-version ${var.kubernetes_version} \
-      --k3s-extra-args '--disable=traefik --node-external-ip=${var.external_ip} --advertise-address=${var.ssh_server_address} --node-ip=${var.ssh_server_address}'
+      --k3s-extra-args "--disable=traefik --node-external-ip=${var.external_ip} --advertise-address=${var.internal_ip} --node-ip=${var.internal_ip}"
 
     rm -f /tmp/id_ed25519
     EOT
   }
+  depends_on = [ null_resource.check_hostname ]
 }
+
+
 
 # resource "null_resource" "nfs_server" {
 #   count = var.storage == "local-path" ? 1 : 0
--- deploy/terraform/k3s/variables.tf
+++ deploy/terraform/k3s/variables.tf
@@ -8,14 +8,24 @@ variable "ssh_private_key" {
   default = "~/.ssh/id_ed25519"
   description = "The path to the private key to use when connecting to the server via SSH."
 }
+variable "ssh_server_port" {
+  type = string
+  default = "22"
+  description = "The port to use when connecting to the server via SSH."
+}
 variable "kubernetes_version" {
     type = string
-    default = "v1.26.4+k3s1"
+    default = "v1.32.3+k3s1"
     description = "which version of k3s to install, usually 1 versions behind the latest"
 }
 variable "external_ip" {
     type = string
     default = "1.2.3.4"
+    description = "sets the external ip address, a script to update ips and restart k3s is also uploaded to the vm"
+}
+variable "internal_ip" {
+    type = string
+    default = "192.168.1.185"
     description = "sets the external ip address, a script to update ips and restart k3s is also uploaded to the vm"
 }
 variable "ssh_server_address" {
@@ -27,4 +37,8 @@ variable "storage" {
   type = string
   default = "local-path"
   description = "the default storage class"
+}
+variable "hostname" {
+  type        = string
+  description = "Expected hostname of the server"
 }
