9494b9dffe8b0943c4b153974a9ebc479c0d9116
loeken <loeken@internetz.me>
2025-06-05 02:41:18 +0200

    testing default hostname

--- deploy/terraform/k3s-proxmox/main.tf
+++ deploy/terraform/k3s-proxmox/main.tf
@@ -58,6 +58,8 @@ resource "proxmox_virtual_environment_vm" "k3s_vm" {
     dns {
       server = "8.8.8.8 1.1.1.1"
     }
+
+    hostname = "k3snode${format("%02d", count.index+1)}"
     user_account {
       keys     = [trimspace(file("~/.ssh/id_ed25519.pub"))]
       username = var.user_name
