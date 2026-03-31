d76bd546e52ec3aadcd4f08be27247f66c12b578
loeken <loeken@internetz.me>
2026-03-31 08:38:20 +0200

    chore(deps): update dependency k3s-github to v1.35.3+k3s1 (#487)
    Co-authored-by: Renovate Bot <renovate@whitesourcesoftware.com>

--- deploy/terraform/k3s/variables.tf
+++ deploy/terraform/k3s/variables.tf
@@ -15,7 +15,7 @@ variable "ssh_server_port" {
 }
 variable "kubernetes_version" {
     type = string
-    default = "v1.35.2+k3s1"
+    default = "v1.35.3+k3s1"
     description = "which version of k3s to install, usually 1 versions behind the latest"
 }
 variable "external_ip" {
